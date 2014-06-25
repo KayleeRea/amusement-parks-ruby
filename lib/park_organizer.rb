35ad344a2fb3bf8bd23886708b2c18aa96733a7e
Kaylee Edmonson <kaylee.edmonson@gmail.comcore.autocrlf>
2014-06-25 17:26:44 -0600

    Can sort amusement parks by id, country and country and state

--- lib/park_organizer.rb
+++ lib/park_organizer.rb
@@ -2,9 +2,9 @@ class ParkOrganizer
 
   def by_id(input)
     hash = {}
+
     input.each do |info|
       id = info.first[1]
-      #hash = {id => info}
       hash[id] = info
     end
     hash
@@ -13,19 +13,22 @@ class ParkOrganizer
   def by_country(input)
     hash = {}
     input.each do |info|
-      country =  info.values.last
-      hash[country] = info
+      country = info.values.last
+      hash[country] ||= []
+      hash[country] << info
     end
     hash
   end
 
-    def by_state_and_country(input)
-      hash = {}
-       input.each do |info|
-        country = info.values[3]
-        state = info.values.last
-        hash[country + ', ' + state] = [info]
-       end
-      hash
+  def by_state_and_country(input)
+    hash = {}
+    input.each do |info|
+      info
+      country = info.values[3]
+      state = info.values.last
+      hash[country + ', ' + state] ||= []
+      hash[country + ', ' + state] << info
     end
+    hash
+  end
 end
